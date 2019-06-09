a30a02a29b5958a0e4116dfd6db47e6ae8003f90
bezawit009@gmail.com <bezawit009@gmail.com>
2019-06-09 20:42:49 +0300

    day commit

---
+++ day4/day4.js
@@ -0,0 +1,16 @@
+var book = {
+  titile:" sememen",
+  autor:"j.mk",
+ relaseDate:"2005",
+}
+  
+   console.log(book.titile)
+   console.log(book.autor)
+   console.log(book.relaseDate)
+   
+
+//food
+for(var i=0; i < 3; i++)
+var food = ["pasta", "brade","mmm"];
+console.log(fruits.length); 
+
